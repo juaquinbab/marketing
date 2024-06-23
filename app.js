4d1ba37c091d62dd09572de1d25264220bf1e4de
creativo <camilo.jimenez.r@hotmail.com>
2024-06-23 12:17:23 -0500

    commit231217

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ client.on('authenticated', (session) => {
 
 
  const mediaFilemp3 = MessageMedia.fromFilePath(`./public/media/${'1.jpeg'}`)
-  const mediaFilemp4 = MessageMedia.fromFilePath(`./public/media/${'1.mp3'}`)
+  const mediaFilemp4 = MessageMedia.fromFilePath(`./public/media/${'5.mp3'}`)
 //const mediaFilepdf = MessageMedia.fromFilePath(`./public/media/${'CATÁLOGO_ABRIL .pdf'}`)
 
 
